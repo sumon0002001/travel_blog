da07c9ad7edc15e59106f949c8146708f8d38f33
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-01-11 03:13:07 +0530

    addinf nav bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { cx } from "@/utils";
 import "./globals.css";
 import { Inter, Manrope } from "next/font/google";
 
@@ -22,7 +23,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${manrope.variable} font-mr bg-light dark:bg-dark`}
+        className={cx(
+          inter.variable,
+          manrope.variable,
+          "font-mr bg-light dark:bg-dark"
+        )}
       >
         {children}
       </body>
