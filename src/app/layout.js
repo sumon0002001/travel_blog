34a38789a642158bd34b3300a7a8630cc8506449
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-01-11 01:09:29 +0530

    Add family font

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${manrope.variable} `}>{children}</body>
     </html>
   )
 }
